72ac4ab03c75e7b3617df350cec04034acd01d10
leminhnguyen <leminhnguyen.bk@gmail.com>
2020-11-21 18:30:20 +0700

    add test package

---
+++ test/ValidatePhoneNumberTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ValidatePhoneNumberTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
